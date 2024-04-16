7bc287953dded7aba34098ea78dba304bfe1b2b3
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2024-04-16 17:08:20 -0400

    Update Error Handling
    Co-authored-by: Cesar Gonzalez <cesar.a.gonzalezcs@gmail.com>

--- apps/browser/src/platform/messaging/chrome-message.sender.ts
+++ apps/browser/src/platform/messaging/chrome-message.sender.ts
@@ -2,6 +2,16 @@ import { LogService } from "@bitwarden/common/platform/abstractions/log.service"
 import { CommandDefinition, MessageSender } from "@bitwarden/common/platform/messaging";
 import { getCommand } from "@bitwarden/common/platform/messaging/internal";
 
+type ErrorHandler = (logger: LogService, command: string) => void;
+
+const HANDLED_ERRORS: Record<string, ErrorHandler> = {
+  "Could not establish connection. Receiving end does not exist.": (logger, command) =>
+    logger.debug(`Receiving end didn't exist for command '${command}'`),
+
+  "The message port closed before a response was received.": (logger, command) =>
+    logger.debug(`Port was closed for command '${command}'`),
+};
+
 export class ChromeMessageSender implements MessageSender {
   constructor(private readonly logService: LogService) {}
 
@@ -12,21 +22,15 @@ export class ChromeMessageSender implements MessageSender {
     const command = getCommand(commandDefinition);
     chrome.runtime.sendMessage(Object.assign(payload, { command: command }), () => {
       if (chrome.runtime.lastError) {
-        if (
-          chrome.runtime.lastError.message ===
-          "Could not establish connection. Receiving end does not exist."
-        ) {
-          this.logService.debug(`Receiving end didn't exist for command '${command}'`);
-        } else if (
-          chrome.runtime.lastError.message ===
-          "The message port closed before a response was received."
-        ) {
-          this.logService.debug(`Port was closed for command '${command}'`);
-        } else {
-          this.logService.warning(
-            `Error while sending message with command '${command}': ${chrome.runtime.lastError.message}`,
-          );
+        const errorHandler = HANDLED_ERRORS[chrome.runtime.lastError.message];
+        if (errorHandler != null) {
+          errorHandler(this.logService, command);
+          return;
         }
+
+        this.logService.warning(
+          `Unhandled error while sending message with command '${command}': ${chrome.runtime.lastError.message}`,
+        );
       }
     });
   }
