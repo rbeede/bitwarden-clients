f8229553ee9b9ad36fabeca76ea4d070ec9a28a6
Todd Martin <tmartin@bitwarden.com>
2024-04-18 14:59:36 -0400

    Fixed tests

--- apps/web/src/app/auth/key-rotation/user-key-rotation.service.spec.ts
+++ apps/web/src/app/auth/key-rotation/user-key-rotation.service.spec.ts
@@ -187,16 +187,6 @@ describe("KeyRotationService", () => {
       );
     });
 
-    it("uses legacy rotation if feature flag is off", async () => {
-      mockConfigService.getFeatureFlag.mockResolvedValueOnce(false);
-
-      await keyRotationService.rotateUserKeyAndEncryptedData("mockMasterPassword");
-
-      expect(mockApiService.postUserKeyUpdate).toHaveBeenCalled();
-      expect(mockEmergencyAccessService.postLegacyRotation).toHaveBeenCalled();
-      expect(mockResetPasswordService.postLegacyRotation).toHaveBeenCalled();
-    });
-
     it("throws if server rotation fails", async () => {
       mockApiService.postUserKeyUpdate.mockRejectedValueOnce(new Error("mockError"));
 
