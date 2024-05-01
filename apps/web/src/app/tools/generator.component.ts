21999529693e793335eb4a49fbe7c6dfb110c748
✨ Audrey ✨ <ajensen@bitwarden.com>
2024-05-01 17:39:01 -0400

    fix incorrect constructor arguments

--- apps/web/src/app/tools/generator.component.ts
+++ apps/web/src/app/tools/generator.component.ts
@@ -1,12 +1,11 @@
-import { ChangeDetectorRef, Component, NgZone } from "@angular/core";
+import { Component } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 
 import { GeneratorComponent as BaseGeneratorComponent } from "@bitwarden/angular/tools/generator/components/generator.component";
-import { BroadcasterService } from "@bitwarden/common/platform/abstractions/broadcaster.service";
+import { AccountService } from "@bitwarden/common/auth/abstractions/account.service";
 import { I18nService } from "@bitwarden/common/platform/abstractions/i18n.service";
 import { LogService } from "@bitwarden/common/platform/abstractions/log.service";
 import { PlatformUtilsService } from "@bitwarden/common/platform/abstractions/platform-utils.service";
-import { StateService } from "@bitwarden/common/platform/abstractions/state.service";
 import { PasswordGenerationServiceAbstraction } from "@bitwarden/common/tools/generator/password";
 import { UsernameGenerationServiceAbstraction } from "@bitwarden/common/tools/generator/username";
 import { DialogService } from "@bitwarden/components";
@@ -21,27 +20,21 @@ export class GeneratorComponent extends BaseGeneratorComponent {
   constructor(
     passwordGenerationService: PasswordGenerationServiceAbstraction,
     usernameGenerationService: UsernameGenerationServiceAbstraction,
-    stateService: StateService,
+    accountService: AccountService,
     platformUtilsService: PlatformUtilsService,
     i18nService: I18nService,
     logService: LogService,
     route: ActivatedRoute,
-    broadcasterService: BroadcasterService,
-    ngZone: NgZone,
-    changeDetectorRef: ChangeDetectorRef,
     private dialogService: DialogService,
   ) {
     super(
       passwordGenerationService,
       usernameGenerationService,
       platformUtilsService,
-      stateService,
+      accountService,
       i18nService,
       logService,
       route,
-      broadcasterService,
-      ngZone,
-      changeDetectorRef,
       window,
     );
     if (platformUtilsService.isSelfHost()) {
