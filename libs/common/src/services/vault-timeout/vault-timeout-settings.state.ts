c07a1246ca4ccdf543de714738e5f9030ec32bbd
Jared Snider <jsnider@bitwarden.com>
2024-05-02 14:06:24 -0400

    PM-5501 - Per PR feedback, add more context as to why we need vault timeout settings to persist after logout.

--- libs/common/src/services/vault-timeout/vault-timeout-settings.state.ts
+++ libs/common/src/services/vault-timeout/vault-timeout-settings.state.ts
@@ -3,7 +3,10 @@ import { UserKeyDefinition, VAULT_TIMEOUT_SETTINGS_DISK_LOCAL } from "../../plat
 import { VaultTimeout } from "../../types/vault-timeout.type";
 
 /**
- * Settings use disk storage and local storage on web so settings can persist after logout.
+ * Settings use disk storage and local storage on web so settings can persist after logout
+ * in order for us to know if the user's chose to never lock their vault or not.
+ * When the user has never lock selected, we have to set the user key in memory
+ * from the user auto unlock key stored on disk on client bootstrap.
  */
 export const VAULT_TIMEOUT_ACTION = new UserKeyDefinition<VaultTimeoutAction>(
   VAULT_TIMEOUT_SETTINGS_DISK_LOCAL,
